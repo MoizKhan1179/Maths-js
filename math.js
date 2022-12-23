8968788996196d62b076572e0ef3999647f998c7
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-23 19:00:35 +0500

    Add files via upload

---
+++ math.js
@@ -0,0 +1,105 @@
+// **********isFinite**********
+
+// let a = 5**1000
+
+// let b = Number.isFinite(a)
+// console.log(`Before : `,a)
+// console.log(`After : `,b)
+
+
+// ********isInteger********
+
+// let a = 78
+// console.log(`Before : `,a)
+// let b = Number.isSafeInteger(a)
+// console.log(`After : `,b)
+
+
+// *********toFixed*********  <point k bad>
+
+// let a = 5135486494.3241574879895621215786131215
+
+// let b = a.toFixed(4)
+// // console.log(`Before : `,a)
+// console.log(`After : `,b)
+
+// *********toPrecision***********
+
+// let a = 78.8798520
+// console.log(a)
+// let b = a.toPrecision(3)
+// console.log(b)
+
+
+// *********ceil**********
+
+// let a = 78.6589
+// console.log(a)
+// let b = Math.ceil(a)
+// console.log(b)
+
+
+// *********Floor**********
+
+// let a = 78.6578
+// console.log(a)
+// let b = Math.floor(a)
+// // console.log(b)
+
+
+// **********Round************
+
+// let a = 6.4
+// console.log(a)
+// let b = Math.round(a)
+// console.log(b)
+
+
+// *******Trunc********   <decimal ko integer ma show kre ga>
+
+// let a = 78.689
+// console.log(a)
+// let b = Math.trunc(a)
+// console.log(b)
+
+
+
+// *******Maximum*******
+
+// console.log(Math.max(36,25,78,356,98,18,9812,97,44,54,9,45,1,49,4))
+
+
+// ********Minimum**********
+
+// console.log(Math.min(56,2,6,4594,51,21,874,87,512))
+
+
+
+
+
+// *************squareRoot*************
+
+// console.log(Math.sqrt(35))
+
+
+
+
+
+
+// *************cubeRoot*************
+
+// console.log(Math.sqrt(35))
+
+
+
+
+
+// *************Power*************
+
+// console.log(Math.pow(2*4))
+
+
+
+// *************random*************
+
+// console.log(Math.round(Math.random()*10+1))
